993e012a1f200688e3b1d10b3c6b91c582d97605
DiggsPapu <alo20172@uvg.edu.gt>
2022-06-02 13:20:46 -0700

    Python terminal

--- Proyect2RecomendationSystem-master/Others/Main.py
+++ Proyect2RecomendationSystem-master/Others/Main.py
@@ -14,7 +14,7 @@ df = df_list[-1]
 df2 = df_list[-8]
 # print(df2)
 #df.to_csv(r'C:\Users\Administrator\Documents\Proyecto\Proyecto2\Proyect2RecomendationSystem-master\Others\data.csv', index=False)
-greeter = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "Manager123"))
+greeter = GraphDatabase.driver("neo4j+s://c5279997.databases.neo4j.io", auth=("neo4j", "LPXUNTzhTSuSg6dyX_7Ip6tAchtJkqQL0DqZCnBczPM"))
 with greeter.session() as session:
     query2 = "match(a:User)-[r:Agresividad_Para_Invertir]->() delete r"
     session.run(query2)
@@ -179,18 +179,18 @@ while (True):
         name = input("\nIngrese su nombre: ")
         surname = input("\nIngrese su apellido: ")
         array = ["Bancos", "Mineria", "Quimica", "Aerolineas", "Energia", "Transporte", "Mecanica", "Turismo", "Textiles", "Salud", "Telecomunicaciones", "Inmobiliaria"]
-        sector1 = input("\nIngrese un sector que le interese entre: Bancos, Mineria, Quimica, Aerolineas, Energia, Transporte, Mecanica, Turismo, Textiles, Salud, Telecomunicaciones, Inmobiliaria ")
+        sector1 = input("\nIngrese un sector que le interese interese (Bancos, Mineria, Quimica, Aerolineas, Energia, Transporte, Mecanica, Turismo, Textiles, Salud, Telecomunicaciones, Inmobiliaria): ")
        
         while sector1 not in array:
-            sector1 = input("\nIngrese un sector valido entre: Bancos, Mineria, Quimica, Aerolineas, Energia, Transporte, Mecanica, Turismo, Textiles, Salud, Telecomunicaciones, Inmobiliaria ")
+            sector1 = input("\nIngrese un sector valido interese (Bancos, Mineria, Quimica, Aerolineas, Energia, Transporte, Mecanica, Turismo, Textiles, Salud, Telecomunicaciones, Inmobiliaria): ")
         
-        sector2 = input("\nIngrese un sector que le interese entre: Bancos, Mineria, Quimica, Aerolineas, Energia, Transporte, Mecanica, Turismo, Textiles, Salud, Telecomunicaciones, Inmobiliaria ")
+        sector2 = input("\nIngrese un sector que le interese (Bancos, Mineria, Quimica, Aerolineas, Energia, Transporte, Mecanica, Turismo, Textiles, Salud, Telecomunicaciones, Inmobiliaria): ")
         while sector2 not in array:
-            sector2 = input("\nIngrese un sector valido entre: Bancos, Mineria, Quimica, Aerolineas, Energia, Transporte, Mecanica, Turismo, Textiles, Salud, Telecomunicaciones, Inmobiliaria ")
+            sector2 = input("\nIngrese un sector valido interese (Bancos, Mineria, Quimica, Aerolineas, Energia, Transporte, Mecanica, Turismo, Textiles, Salud, Telecomunicaciones, Inmobiliaria): ")
         
-        sector3 = input("\nIngrese un sector que le interese entre: Bancos, Mineria, Quimica, Aerolineas, Energia, Transporte, Mecanica, Turismo, Textiles, Salud, Telecomunicaciones, Inmobiliaria ")
+        sector3 = input("\nIngrese un sector que le interese (Bancos, Mineria, Quimica, Aerolineas, Energia, Transporte, Mecanica, Turismo, Textiles, Salud, Telecomunicaciones, Inmobiliaria): ")
         while sector3 not in array:
-            sector3 = input("\nIngrese un sector valido entre: Bancos, Mineria, Quimica, Aerolineas, Energia, Transporte, Mecanica, Turismo, Textiles, Salud, Telecomunicaciones, Inmobiliaria ")
+            sector3 = input("\nIngrese un sector valido interese (Bancos, Mineria, Quimica, Aerolineas, Energia, Transporte, Mecanica, Turismo, Textiles, Salud, Telecomunicaciones, Inmobiliaria): ")
 
         riesgo = 4839201
         while riesgo not in range (0,6):
